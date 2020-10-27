6f8071159777cd0113b323598901ae64ff9a1cd2
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-10-27 16:20:22 -0500

    Suscribiendo otro Observador y modificando el Stream reactivo

--- src/main/java/com/dipper/springboot/webflux/app/controllers/ProductoController.java
+++ src/main/java/com/dipper/springboot/webflux/app/controllers/ProductoController.java
@@ -2,6 +2,8 @@ package com.dipper.springboot.webflux.app.controllers;
 
 import com.dipper.springboot.webflux.app.models.dao.ProductoDao;
 import com.dipper.springboot.webflux.app.models.documents.Producto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,12 +17,19 @@ import reactor.core.publisher.Flux;
  */
 @Controller
 public class ProductoController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+
     @Autowired
     private ProductoDao productoDao;
 
     @GetMapping({"/listar", "/"})
     public String listar(Model model) {
-        Flux<Producto> productos = productoDao.findAll();
+        Flux<Producto> productos = productoDao.findAll().map(producto -> {
+            producto.setNombre(producto.getNombre().toUpperCase());
+            return producto;
+        });
+        productos.subscribe(prod -> LOGGER.info(prod.getNombre()));
         model.addAttribute("productos", productos);
         model.addAttribute("titulo", "Listado de productos");
         return "listar";
